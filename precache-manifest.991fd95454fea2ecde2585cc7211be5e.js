bae629170ff7daa2cca26f010f123b9b321bfb45
daniel molina <bigdpower@icloud.com>
2020-07-10 15:21:28 -0700

    Updates

---
+++ precache-manifest.991fd95454fea2ecde2585cc7211be5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c291cdea4bdcc5a399e8c1029bb7ce26",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "e08a416695290b6a2b49",
+    "url": "/react-portfolio/static/css/main.50e1e22a.chunk.css"
+  },
+  {
+    "revision": "3095d3f0739d0cc93cdf",
+    "url": "/react-portfolio/static/js/2.615daff4.chunk.js"
+  },
+  {
+    "revision": "9a9e56582fbb6743dfc4c1baab3b2d6f",
+    "url": "/react-portfolio/static/js/2.615daff4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e08a416695290b6a2b49",
+    "url": "/react-portfolio/static/js/main.a17b171d.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  }
+]);
