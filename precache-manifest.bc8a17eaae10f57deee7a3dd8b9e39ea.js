41cd7408ffa3b7e4a917a2c497576c262890db6d
daniel molina <bigdpower@icloud.com>
2021-12-31 04:52:01 -0700

    Updates

---
+++ precache-manifest.bc8a17eaae10f57deee7a3dd8b9e39ea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0e1a4672ec13ab04e00b9df5749465e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "742bb6f96ab0a837c284",
+    "url": "/static/css/2.eb43dee1.chunk.css"
+  },
+  {
+    "revision": "b61bde4384dc0ad1475c",
+    "url": "/static/css/main.602407af.chunk.css"
+  },
+  {
+    "revision": "742bb6f96ab0a837c284",
+    "url": "/static/js/2.686edba0.chunk.js"
+  },
+  {
+    "revision": "85658634e0448320de6bbd243656f33a",
+    "url": "/static/js/2.686edba0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b61bde4384dc0ad1475c",
+    "url": "/static/js/main.b55b3b0f.chunk.js"
+  },
+  {
+    "revision": "dd8147bbe7734801bdde",
+    "url": "/static/js/runtime-main.e312f208.js"
+  },
+  {
+    "revision": "fb7285a3902e8261c1f75ee478c23f85",
+    "url": "/static/media/Resume 2021.fb7285a3.pdf"
+  }
+]);
