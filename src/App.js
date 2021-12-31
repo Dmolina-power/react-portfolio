7861f4aafa56eb37d6fd29e336d0cb2770aae144
daniel molina <bigdpower@icloud.com>
2021-12-31 02:27:24 -0700

    fixed errors

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Router, Route } from "react-router-dom";
 
 //styles
 import "./App.css";
@@ -13,8 +13,8 @@ const App = () => {
   return (
     <Router>
       <Route exact path="/" component={Home} />
-      <Route exact path="/about" component={About} />
-      <Route exact path="/portfolio" component={Portfolio} />
+      <Route exact path="#about" component={About} />
+      <Route exact path="#portfolio" component={Portfolio} />
     </Router>
   );
 }
