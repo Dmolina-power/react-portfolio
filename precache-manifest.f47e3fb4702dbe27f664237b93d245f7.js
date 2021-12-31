6feb374c805dacb03c3366552aa3545bec7956cf
daniel molina <bigdpower@icloud.com>
2021-12-31 04:38:35 -0700

    Updates

---
+++ precache-manifest.f47e3fb4702dbe27f664237b93d245f7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5fd7c9be860aeebf2b413eacf6814a8",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "742bb6f96ab0a837c284",
+    "url": "/react-portfolio/static/css/2.eb43dee1.chunk.css"
+  },
+  {
+    "revision": "299333f5ba05465cf9ec",
+    "url": "/react-portfolio/static/css/main.602407af.chunk.css"
+  },
+  {
+    "revision": "742bb6f96ab0a837c284",
+    "url": "/react-portfolio/static/js/2.686edba0.chunk.js"
+  },
+  {
+    "revision": "85658634e0448320de6bbd243656f33a",
+    "url": "/react-portfolio/static/js/2.686edba0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "299333f5ba05465cf9ec",
+    "url": "/react-portfolio/static/js/main.2e1532f1.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "fb7285a3902e8261c1f75ee478c23f85",
+    "url": "/react-portfolio/static/media/Resume 2021.fb7285a3.pdf"
+  }
+]);
